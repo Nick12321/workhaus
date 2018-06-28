ea3455e1527e6a5b32181adfb93354ad33685ddc
Nick12321 <Nick.thompson@rogers.com>
2018-06-28 00:54:42 -0400

    original version

--- checkPalindrome.py
+++ checkPalindrome.py
@@ -1,39 +1,22 @@
-def isPalindrome(word):
+def checkPalindrome(word):
     word = word.lower()
     word = word.replace(' ', '')
     lenOfOriginalWord = len(word)
     if lenOfOriginalWord == 0:
-        return False
+        return "Not Palindrome"
 
     remainder = 1
     if lenOfOriginalWord % 2 == 0:
         remainder = 0
     while len(word) != remainder:
         firstCharacter = word[0]
-        lastCharacter = word[len(word) - 1]
+        lastCharacter = [len(word) - 1]
         if firstCharacter != lastCharacter:
-            return False
-        word = word[1 : len(word) -1]
-    return True
+            return "Not a Palindrome"
+        word = word[1 : len(word) - 1]
+    return "Palindrome"
 
-def longestPalindrome(word):
-    largestPalindrome = 0
-    for i in range(0, len(word)):
-        for j in range(i, len(word)):
-            subStringOfWord = word[i:j+1]
-
-
-            if isPalindrome(subStringOfWord):
-                lenOfSubStringOfWord = len(subStringOfWord)
-
-                if largestPalindrome < lenOfSubStringOfWord:
-                    largestPalindrome = lenOfSubStringOfWord
-                    palString = subStringOfWord
-    print(palString)
-    return largestPalindrome
-
-
-print(longestPalindrome('aaathjmk'))
-print(longestPalindrome('banana'))
-print(longestPalindrome('asdfghgfdsa'))
-print(longestPalindrome('alakajahagaf'))
+print(checkPalindrome('Anna'))
+print(checkPalindrome('bob'))
+print(checkPalindrome('boxb'))
+print(checkPalindrome(' '))
