0c39400d6e256a12d12a2189728f5b51d8d017ba
Nick12321 <Nick.thompson@rogers.com>
2018-06-28 08:26:08 -0400

    first commit - warehouse automation

---
+++ warehouse.py
@@ -0,0 +1,11 @@
+import datetime
+DATE = datetime.datetime.now()
+
+class Item:
+    def __init__(self, itemName, label, postalCode, area):
+        self.itemName = name
+        self.label = label
+        self.postalCode = code
+        self.area = area
+
+    def addItem:
