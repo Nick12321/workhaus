003d56933b02980471b98070fe736e8dfa4a3a53
Nick12321 <Nick.thompson@rogers.com>
2018-06-28 00:59:42 -0400

    added check for longest Palindrome string

--- checkPalindrome.py
+++ checkPalindrome.py
@@ -3,7 +3,7 @@ def checkPalindrome(word):
     word = word.replace(' ', '')
     lenOfOriginalWord = len(word)
     if lenOfOriginalWord == 0:
-        return "Not Palindrome"
+        return False
 
     remainder = 1
     if lenOfOriginalWord % 2 == 0:
@@ -12,11 +12,28 @@ def checkPalindrome(word):
         firstCharacter = word[0]
         lastCharacter = [len(word) - 1]
         if firstCharacter != lastCharacter:
-            return "Not a Palindrome"
+            return False
         word = word[1 : len(word) - 1]
-    return "Palindrome"
+    return True
 
-print(checkPalindrome('Anna'))
-print(checkPalindrome('bob'))
-print(checkPalindrome('boxb'))
-print(checkPalindrome(' '))
+def longestPalindrome(word):
+    largestPalindrome = 0
+    for i in range(0, len(word)):
+        for j in range(i, len(word)):
+            subStringOfWord = word[i:j+1]
+
+
+            if isPalindrome(subStringOfWord):
+                lenOfSubStringOfWord = len(subStringOfWord)
+
+                if largestPalindrome < lenOfSubStringOfWord:
+                    largestPalindrome = lenOfSubStringOfWord
+                    palString = subStringOfWord
+    print(palString)
+    return largestPalindrome
+
+
+print(longestPalindrome('aaathjmk'))
+print(longestPalindrome('banana'))
+print(longestPalindrome('asdfghgfdsa'))
+print(longestPalindrome('alakajahagaf'))
