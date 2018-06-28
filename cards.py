27dc6f94f2db48e019a4077ee0c83f4f47c0328a
Nick12321 <Nick.thompson@rogers.com>
2018-06-28 00:33:06 -0400

    Updated class Deck w Random and shuffle

--- cards.py
+++ cards.py
@@ -18,6 +18,23 @@ class Deck:
             for rank in self.ranks:
                 card = Card(suit, rank)
                 self.deck.append(card)
+        import random
+        length = len(self.deck)
+        self.random = []
+        self.random = self.deck
+        while length != 0:
+            position = random.randint(0, (length - 1))
+            randomCard = self.random[position]
+            randomCard.printCard()
+            del self.random[position]
+            length -= 1
+        print('==========')
+        randomCount = len(self.random)
+        deckCount = len(self.deck)
+        print("Number of cards left in Random:")
+        print(randomCount)
+        print("Number of cards in Deck:")
+        print(deckCount)
 
     def printDeck(self):
         for card in self.deck:
