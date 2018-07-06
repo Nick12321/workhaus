179293d34e73098397bd42941e1dea50901f4a72
Nick Thompson <Nick.thompson@rogers.com>
2018-07-06 19:04:36 -0400

    linked list w slideshow functionality

---
+++ slideshow.py
@@ -0,0 +1,7 @@
+
+
+item1 = "Four score and twaenty years ago"
+item2 = "Get busy living or get busy dieing"
+item3 = "Life is what happens when you're busy making other plans"
+item4 = "Twenty years from now you will be more disappointed by the things you didnt do than the things you did do"
+item5 = "Great minds discuss ideas; average minds discuss events; small minds discuss people"
