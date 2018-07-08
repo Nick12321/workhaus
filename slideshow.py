bddd8542f9128501de4d9b4f51bbcf62006b398f
Nick12321 <Nick.thompson@rogers.com>
2018-07-08 13:35:49 -0400

    Node and linkedList classes updated

--- slideshow.py
+++ slideshow.py
@@ -1,31 +1,63 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+class Node(nodeObject):
 
+    def __init__(self, data=None, nextNode=None):
+        self.data = data
+        self.nextNode = nextNode
 
-class linkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
+    def getData(self):
+        return self.data
 
-    def addItemToLinkedList(self, value):
-        newNode = Node(value)
-        print('in addItemToLinkedList')
-        if self.tail == None:
-            self.tail = newNode
-        else:
-            self.tail.next = newNode
-            self.tail = newNode
-        print(newNode)
+    def getNext(self):
+        return self.nextNode
 
-    def printLinkedList(self):
-        print('in printLinkedList')
-        currentNode = self.head
-        while currentNode != None:
-            print(currentNode.value)
-            currentNode = currentNode.next
+    def setNext(self, newNext):
+        self.nextNode = newNext
 
+class linkedList(nodeObject):
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert(self, data):
+        newNode = Node(data)
+        newNode.setNext(self.head)
+        self.head = newNode
+
+    def size(self):
+        current = self.head
+        counter = 0
+        while current:
+            count += 1
+            current = current.getNext()
+        return count
+
+    def search(self, data):
+        current = self.head
+        found = False
+        while current and found is False:
+            if current.getData() == data:
+                found = True
+            else:
+                current = current.getNext()
+            if current is None:
+                raise ValueError("Data not in list")
+            return current
+
+    def delete(self, data):
+        current = self.head
+        previous = None
+        found = False
+        while current and found is False:
+            if current.getData() == data:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+            if current is None:
+                raise ValueError("Data not in list")
+            if previous is None:
+                self.head = current.getNext()
+            else:
+                previous.setNext(current.getNext())
 
 
 list = linkedList()
@@ -34,10 +66,9 @@ body2 = "Get busy living or get busy dieing"
 body3 = "Life is what happens when you're busy making other plans"
 body4 = "Twenty years from now you will be more disappointed by the things you didnt do than the things you did do"
 body5 = "Great minds discuss ideas; average minds discuss events; small minds discuss people"
-list.addItemToLinkedList(body1)
-list.addItemToLinkedList(body2)
-list.addItemToLinkedList(body3)
-list.addItemToLinkedList(body4)
-list.addItemToLinkedList(body5)
+list.insert(body1)
+list.insert(body2)
+list.insert(body3)
+list.insert(body4)
+list.insert(body5)
 print('here')
-list.printLinkedList()
